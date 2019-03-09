71d76a095b244ba554b177b5c640acd75e0d839e
Gabriel Napoleão <gabriel.ndr.sky@gmail.com>
2019-03-09 17:52:47 -0300

    Some modifications

---
+++ VideoPictureInPicture.vue
@@ -0,0 +1,68 @@
+<template>
+  <p>Hello {{name}}</p>
+</template>
+
+<script>
+
+
+let mousePosition
+let offset = [0,0]
+let div
+let isDown = false
+
+div = document.createElement('div')
+div.style.position = 'absolute'
+div.style.left = '0px'
+div.style.top = '0px'
+div.style.width = '100px'
+div.style.height = '100px'
+div.style.background = 'red'
+div.style.color = 'blue'
+
+document.body.appendChild(div)
+
+div.addEventListener('mousedown', event => {
+    isDown = true
+    offset = [
+        div.offsetLeft - event.clientX,
+        div.offsetTop - event.clientY
+    ]
+})
+
+document.addEventListener('mouseup', () => {
+    isDown = false
+})
+
+document.addEventListener('mousemove', event => {
+    event.preventDefault()
+    if (isDown) {
+        mousePosition = {
+            x: event.clientX,
+            y: event.clientY
+        }
+
+        let left = (mousePosition.x + offset[0])
+        let top = (mousePosition.y + offset[1])
+
+        //if (left >= 0 && left <= (document.body.offsetWidth))
+          div.style.left = `${left}px`
+          
+        //if (top >= 0 && top <= document.body.offsetWidth)
+          div.style.top = `${top}px`
+    }
+})
+
+
+
+export default {
+  data() {
+    return {
+      name: 'Gabriel'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ index.vue
@@ -0,0 +1,17 @@
+<template>
+  <video-picture-in-picture></video-picture-in-picture>
+</template>
+
+<script>
+import VideoPictureInPicture from './VideoPictureInPicture.vue'
+
+export default {
+  components: {
+    VideoPictureInPicture
+  }
+}
+</script>
+
+<style>
+
+</style>
